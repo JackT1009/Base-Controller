8135387a31b8b080f43221ec3778fd54b7a41674
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-22 20:05:36 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,20 +1,29 @@
 -- server/main.lua
-local modem = peripheral.find("modem") or error("Need modem")
+local modem = peripheral.find("modem") or error("Modem required")
 rednet.open(peripheral.getName(modem))
 
-print("Server ID: "..os.getComputerID())
+print("Base Control Server Online")
+print("ID: "..os.getComputerID())
 
 while true do
-    local id, message = rednet.receive()
+    local sender, message = rednet.receive()
     local response
     
-    if message == "PING" then
-        print("Received ping from "..id)
-        response = "PONG from Floor 1 Server"
+    -- Registration
+    if message == "REGISTER" then
+        print("New terminal: "..sender)
+        response = "REG_OK-"..sender
+    
+    -- Light control
+    elseif message:find("LIGHTS-") then
+        local state = message:sub(8)
+        rs.setOutput("left", state == "ON")
+        response = "LIGHTS-"..(state == "ON" and "ON" or "OFF")
+    
     end
     
     if response then
-        rednet.send(id, response)
-        print("Sent response to "..id)
+        rednet.send(sender, response)
+        print("Sent: "..response)
     end
 end
