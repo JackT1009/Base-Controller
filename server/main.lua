fa39d044b7544907119242b6d5e06b7c7b14b1aa
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-22 19:53:40 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,20 @@
+-- server/main.lua
+local modem = peripheral.find("modem") or error("Need modem")
+rednet.open(peripheral.getName(modem))
 
+print("Server ID: "..os.getComputerID())
+
+while true do
+    local id, message = rednet.receive()
+    local response
+    
+    if message == "PING" then
+        print("Received ping from "..id)
+        response = "PONG from Floor 1 Server"
+    end
+    
+    if response then
+        rednet.send(id, response)
+        print("Sent response to "..id)
+    end
+end
