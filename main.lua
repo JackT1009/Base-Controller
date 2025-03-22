c6587fcf01298262f9ca410fd12e7fd4155bec47
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-22 21:16:45 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,12 @@
--- server/main.lua
 local core = require("modules/core")
 
 rednet.open("right")
 print("Server ID: "..os.getComputerID())
 
 while true do
-    local id, msg = core.receive()
-    if msg then
-        print("From "..msg.sender..": "..msg.command)
-        core.send(id, core.createMessage(
-            "server",
-            "ACK",
-            "Received: "..msg.command
-        ))
+    local id, message = core.receive()
+    if message then
+        print("Received from "..message.sender..": "..message.command)
+        core.send(id, core.createMessage("Server", "ACK", message.command))
     end
 end
