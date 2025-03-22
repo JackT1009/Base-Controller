47a033718a1a9fa7ef58b728231b7de73f49b215
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-22 20:05:58 +0000

    Update terminal.lua

--- terminals/terminal.lua
+++ terminals/terminal.lua
@@ -1,25 +1,36 @@
 -- terminals/terminal.lua
-local modem = peripheral.find("modem") or error("Need modem")
+local modem = peripheral.find("modem") or error("Modem required")
 local monitor = peripheral.find("monitor") or term
 rednet.open(peripheral.getName(modem))
 
+-- Setup
 monitor.clear()
+monitor.setTextScale(0.5)
 monitor.setCursorPos(1,1)
-monitor.write("Floor 1 Terminal\n")
+monitor.write("FLOOR 1 CONTROL\n")
 
+-- Register with server
+rednet.send(1, "REGISTER") -- Send to server ID 1
+local _, response = rednet.receive(5)
+monitor.write("Status: "..(response or "No connection!"))
+
+-- Main interface
 while true do
-    monitor.setCursorPos(1,3)
-    monitor.write("1. Ping Server\n")
-    monitor.write("2. Exit\n")
+    monitor.setCursorPos(1,4)
+    monitor.write("1. Lights On\n")
+    monitor.write("2. Lights Off\n")
     
     local event, side, x, y = os.pullEvent("monitor_touch")
-    if y == 3 then
-        rednet.send(1, "PING") -- Send to server ID 1
-        local _, response = rednet.receive(2) -- Wait 2 seconds
-        monitor.setCursorPos(1,6)
-        monitor.clearLine()
-        monitor.write(response or "No response!")
-    elseif y == 4 then
-        error()
+    
+    if y == 4 then -- Lights On
+        rednet.send(1, "LIGHTS-ON")
+    elseif y == 5 then -- Lights Off
+        rednet.send(1, "LIGHTS-OFF")
     end
+    
+    -- Get response
+    local _, msg = rednet.receive(2)
+    monitor.setCursorPos(1,8)
+    monitor.write("Last response: ")
+    monitor.write(msg or "Timeout!")
 end
