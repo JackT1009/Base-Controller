ffc51f8327916fe0c47cf31031552a628ef5461d
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-23 09:21:31 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,13 @@
 local core = require("modules/core")
 
+-- Open modem and set up
 rednet.open("right")
 print("Server ID: "..os.getComputerID())
 
 while true do
     local id, message = core.receive()
     if message then
-        print("Received from "..message.sender..": "..message.command)
+        print("From "..message.sender..": "..message.command)
         core.send(id, core.createMessage("Server", "ACK", message.command))
     end
 end
